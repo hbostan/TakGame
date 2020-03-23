82b8a0cfa126dd39ef66e9f68f80bfaafdbd883f
hbostann <hbostann@gmail.com>
2020-03-23 17:15:43 +0300

    Started stack logic
    - Board now holds Stack GameObjects instead of Pieces
    - Removed DragN'Drop
    - Stacks stack like stacks :)

--- Assets/Script/Piece.cs
+++ Assets/Script/Piece.cs
@@ -3,5 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Piece : MonoBehaviour {
-  
+  public string color;
+  public void SetColor(string color) {
+    this.color = color;
+  }
 }
